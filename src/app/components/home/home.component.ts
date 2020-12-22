47d5f1f6e313ea172b6c5a309406099fff9bea0c
Linlin <linlin@Linlins-MacBook-Pro.local>
2020-12-22 22:41:00 +1000

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,3 +1,5 @@
+import { GlobalDataSummary } from './../../models/global-data';
+import { DataServiceService } from './../../services/data-service.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  totalConfirmed = 0;
+  totalActive = 0;
+  totalDeaths = 0;
+  totalRecovered = 0;
+  globalData : GlobalDataSummary[];
+
+  constructor(private dataService : DataServiceService) { }
 
   ngOnInit(): void {
+
+    this.dataService.getGlobalData()
+    .subscribe(
+      {
+        next : (result) => {
+          console.log(result);
+          this.globalData = result;
+          result.forEach(cs => {
+            if(!Number.isNaN(cs.confirmed)) {
+              this.totalActive+=cs.active
+              this.totalConfirmed+=cs.confirmed
+              this.totalDeaths+=cs.confirmed
+              this.totalRecovered+=cs.recovered
+            }
+          })
+        }
+      }
+    )
   }
 
 }
